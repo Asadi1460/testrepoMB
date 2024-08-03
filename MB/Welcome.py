b9ab47d1ee11a3598df709b6b75afa004bd0fe01
Asadi1460 <eng.asadi@gmail.com>
2024-08-03 11:50:18 -0400

    class files

---
+++ MB/Welcome.py
@@ -0,0 +1,10 @@
+print('Hello')
+print("What's your name")
+
+name = input()
+
+print('Welcome ', name, '!')
+
+age = int(input("How old are you?"))
+# print(age)
+print('You will be ', age + 1, 'years, this year')
